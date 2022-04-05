d104ffb013cf3052341cc22155308da04df0296e
pavancrazy007 <58016744+pavancrazy007@users.noreply.github.com>
2022-04-05 18:00:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,10 @@
-import './App.css'
+import {Link} from 'react-router-dom'
 
-//These are the lists used in the application. You can move them to any component needed.
-const employmentTypesList = [
-  {
-    label: 'Full Time',
-    employmentTypeId: 'FULLTIME',
-  },
-  {
-    label: 'Part Time',
-    employmentTypeId: 'PARTTIME',
-  },
-  {
-    label: 'Freelance',
-    employmentTypeId: 'FREELANCE',
-  },
-  {
-    label: 'Internship',
-    employmentTypeId: 'INTERNSHIP',
-  },
-]
-
-const salaryRangesList = [
-  {
-    salaryRangeId: '1000000',
-    label: '10 LPA and above',
-  },
-  {
-    salaryRangeId: '2000000',
-    label: '20 LPA and above',
-  },
-  {
-    salaryRangeId: '3000000',
-    label: '30 LPA and above',
-  },
-  {
-    salaryRangeId: '4000000',
-    label: '40 LPA and above',
-  },
-]
-
-// Replace your code here
-const App = () => <div>Hello World</div>
+const App = () => (
+  <div>
+    <Link to="/">home</Link>
+    <Link to="/about">about</Link>
+  </div>
+)
 
 export default App
